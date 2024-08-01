bba2d64614e74b91d5e6fc10d09c633d89558901
Thainaraw <161708587+Thainaraw@users.noreply.github.com>
2024-08-01 16:47:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado= document.querySelector('.caixa-resultado');
+
+const perguntas = [
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "alternativas 1",
+        "alternativas 2"
+        
+    ]
+
+}
+]
+    
+       
+        
