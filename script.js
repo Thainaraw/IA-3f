6e46ea2c4b08e1eb407fe07440988136a8d0ba78
Thainaraw <161708587+Thainaraw@users.noreply.github.com>
2024-08-28 18:27:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [ //serve para abrir lista de perguntas
     {//abre o objeto das perguntas
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1","Alternativa 2"]
+        enunciado: "Você utiliza a inteligência artificial no seu dia a dia?",
+        alternativas: ["Sim","Não"]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1","Alternativa 2"]
+        enunciado: "Você acha que a IA pode ajudar você no seu trabalho?",
+        alternativas: ["sim","Não"]
+    },
+    {
+        enunciado: "Você acha que a IA pode prejudicar as pessoas de alguma forma?",
+        alternativas: ["Sim","Não"]
     }
-    
 ]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+
+}
+mostraPergunta();
