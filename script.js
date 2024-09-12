1d79b936f620d973a5bc66a953bd0c62105357bd
Thainaraw <161708587+Thainaraw@users.noreply.github.com>
2024-09-12 16:44:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,19 @@ const perguntas = [ //serve para abrir lista de perguntas
     {
         enunciado: "Você acha que a IA pode prejudicar as pessoas de alguma forma?",
         alternativas: ["Sim","Não"]
-    }
+    },
+    {
+        enunciado: "Como você descobriu sobre a IA?",
+        alternativas: ["Redes Sociais","Na escola"]
+    },
+    {
+        enunciado: "Você acha que a IA pode mais atrapalhar o aprendizado das criaças no futuro, do que ajudar?",
+        alternativas: ["Sim","Não"]
+    },
+    {
+        enunciado: "Você gosta de utilizar Inteligencias Artificial?",
+        alternativas: ["Sim","Não"]
+    },
 ]
 
 let posicao = 0;
