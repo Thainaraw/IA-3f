06d4e268f626e82dc7849c00291a8d156ba177d6
Thainaraw <161708587+Thainaraw@users.noreply.github.com>
2024-10-02 18:26:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ const perguntas = [ //serve para abrir lista de perguntas
         alternativas: [{texto: "Sim", afirmação:"afirmação da alternativa"},{texto: "Não", afirmação:"afirmação da alternativa"}]
     },
     {
-        enunciado: "Pergunta 3",
-        alternativas: [{texto: "Sim", afirmação:"afirmação da alternativa"},{texto: "Não, afirmação:"afirmação da alternativa"}]
+        enunciado: "Você acha que a IA pode mais atarapalhar o aprendizado das crianças no futuro do que ajudar?",
+        alternativas: [{texto: "Sim", afirmação:"afirmação da alternativa"}, {texto: "Não", afirmação:"afirmação da alternativa"}]
     },
 ]
 
 let posicao = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
@@ -29,10 +30,19 @@ function mostraPergunta(){
 function mostraAlternativas(){
         for (const alternativa of perguntaAtual.alternativas) {
             const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa;
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
             caixaAlternativas.appendChild(botaoAlternativas);
         }
     
 }
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmação;
+    historiaFinal = afirmacoes;
+    posicao++
+    mostraPergunta();
+}
+
 mostraPergunta();
+
